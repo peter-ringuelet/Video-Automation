bbc036bf943e0ac263e05b073a804d9f97f29ef8
Luz Alba Posse <66437312+luzalbaposse@users.noreply.github.com>
2023-10-03 21:18:44 -0300

    Update divider.py
    Changed split_video function in order to enhance modularity of the code.

--- divider.py
+++ divider.py
@@ -1,40 +1,45 @@
 from moviepy.editor import VideoFileClip
 import os
 
+def ensure_result_directory(video_filename):
+    result_path = f"Resultado/{video_filename}"
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
+def calculate_segment_bounds(duration, video_count):
+    if video_count == 1:
+        start = 0
+        end = 60
+    else:
+        start = (video_count - 1) * 58  # Adjusted to avoid overlap
+        end = start + 60
+
+    if end > duration:
+        end = duration
+
+    return start, end
+
+def process_segment(video, start, end, video_filename, video_count):
+    new_clip = video.subclip(start, end)
+    new_filename = f"Resultado/{video_filename}/video{video_count}.mp4"
+    new_clip.write_videofile(new_filename)
+
 def split_video(filename, video_filename):
-    # Asegurarse de que el directorio 'Resultado' exista, sino crearlo
-    if not os.path.exists(f"Resultado/{video_filename}"):
-        os.makedirs(f"Resultado/{video_filename}")
-    
+    if not os.path.exists("Resultado"):
+        os.makedirs("Resultado")
+
     with VideoFileClip(filename) as video:
         duration = int(video.duration)
-        
-        # Contador para los nombres de los videos
         video_count = 1
-        
-        for i in range(0, duration, 60):
-            if i == 0:  # Primer segmento
-                start = 0
-                end = 60
-            else:
-                start = i - 2 * (video_count-1)
-                end = start + 60
 
-            if end > duration:
-                end = duration
-            
-            new_clip = video.subclip(start, end)
-            
-            new_filename = f"Resultado/{video_filename}/video{video_count}.mp4"
-            new_clip.write_videofile(new_filename)
+        ensure_result_directory(video_filename)
+
+        for _ in range(0, duration, 60):
+            start, end = calculate_segment_bounds(duration, video_count)
+            process_segment(video, start, end, video_filename, video_count)
             video_count += 1
 
 if __name__ == "__main__":
-    if not os.path.exists("Resultado"):
-        os.makedirs("Resultado")
-    
-    video_path = "videos/"  # carpeta donde están ubicados los videos
+    video_path = "videos/"
     video_filename = input(f"Ingresa el nombre del video en la carpeta '{video_path}': ")
     split_video(f"videos/{video_filename}.mp4", video_filename)
-
-    # start=0  end= 60          start=55   end=1:55             start 1:55
